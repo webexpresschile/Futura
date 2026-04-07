bda4844a8cc53d8e37713c3ce6b59d89c56ae562
Web Express Chile <holawebexpress@gmail.com>
2026-04-06 21:11:12 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://webexpresschile.github.io',
+  base: '/Futura', // Esto es vital: debe coincidir con el nombre de tu repo
+  output: 'static',
+});
